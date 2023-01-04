78296f8a037e24f59ea817fc10bc90d07becc846
ok <kelsey.collins23@d155.org>
2023-01-04 08:02:37 -0600

    Initial Commit

---
+++ Firebase Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase Notes
+//
+//  Created by KELSEY COLLINS on 1/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
